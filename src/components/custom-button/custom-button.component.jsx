26713239cd5e74338bef68c2cb2e4999a2dd0d20
di-shiro <d.i.shiro@gmail.com>
2019-10-22 12:41:35 +0900

    sec15: remove comment-out in custom-button.component.jsx

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -7,26 +7,3 @@ const CustomButton = ({ children, ...props }) => (
 );
 
 export default CustomButton;
-
-// ---------- ---------- ---------- ---------- ----------
-// import React from 'react';
-
-// import './custom-button.styles.scss';
-
-// const CustomButton = ({
-//   children,
-//   isGoogleSignIn,
-//   inverted,
-//   ...otherProps
-// }) => (
-//   <button
-//     className={`${inverted ? 'inverted' : ''} ${
-//       isGoogleSignIn ? 'google-sign-in' : ''
-//     } custom-button`}
-//     {...otherProps}
-//   >
-//     {children}
-//   </button>
-// );
-
-// export default CustomButton;
